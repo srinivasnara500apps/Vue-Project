793f75bcc36e761cc606203a7a9970eeeeaf4aa8
srinivasnara500apps <137782978+srinivasnara500apps@users.noreply.github.com>
2025-02-23 12:24:56 +0530

    Delete playground directory

--- playground/nuxt.config.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtConfig({
-    modules: ['../src/module', '@nuxtjs/tailwindcss'],
-    devtools: { enabled: true },
-    css: ['~/assets/css/main.css'],
-    tailwindcss: {
-        content: ['./components/**/*.{js,vue,ts}', './pages/**/*.vue'],
-    },
-})
--- playground/pages/index.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div>
-        <SocialStreamsMain></SocialStreamsMain>
-    </div>
-</template>
